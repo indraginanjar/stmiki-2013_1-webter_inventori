20fb29b66300d93ea04da515e8ed5a7d6508c3f8
masugi <mailsugiarto@gmail.com>
2013-11-08 10:46:53 +0700

    Update About-AfterMenu.php

--- views/About-AfterMenu.php
+++ views/About-AfterMenu.php
@@ -1,23 +1,32 @@
 <p>
-Saya ga tau siapa aja anggota kelompoknya, jadi halaman ini saya pasrahkan sepenuhnya ke kamu :D<br />
-  ~indra
+    Anggota Kelompok IV :<br />
+  
 </p>
 <table>
-	<thead>
-		<tr>
-			<th>NoBP</th>
-			<th>Nama</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td>101100172</td>
-			<td>Sugiarto</td>
-		</tr>
-		<tr>
-			<td>101100180</td>
-			<td>Indra Ginanjar</td>
-		</tr>
-	</tbody>
+    <thead>
+        <tr>
+            <th>NoBP</th>
+            <th>Nama</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>101100180</td>
+            <td>Indra Ginanjar</td>
+        </tr>
+        <tr>
+            <td>101100173</td>
+            <td>Sugiarto</td>
+        </tr>
+        <tr>
+            <td>101100199</td>
+            <td>Adriyanto</td>
+        </tr>
+        <tr>
+            <td>101100174</td>
+            <td>Zoel Fadli</td>
+        </tr>
+
+    </tbody>
 </table>
 
